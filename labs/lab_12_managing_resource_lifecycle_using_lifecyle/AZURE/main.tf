c54774fefc72537264caf23119229410c2a80c5f
olekluk <alexander.lukjanenko@gmail.com>
2026-04-07 11:14:35 +0000

    lab 12

--- labs/lab_12_managing_resource_lifecycle_using_lifecyle/AZURE/main.tf
+++ labs/lab_12_managing_resource_lifecycle_using_lifecyle/AZURE/main.tf
@@ -22,3 +22,35 @@ resource "azurerm_storage_account" "standard" {
     Purpose     = "Standard"
   }
 }
+
+
+resource "azurerm_resource_group" "protected" {
+  name     = "rg-protected-${var.environment}"
+  location = var.location
+
+  tags = {
+    Environment = var.environment
+    Purpose     = "Protected"
+  }
+
+  # lifecycle {                      # <--- add this lifecycle block here (all 3 lines)
+  #   prevent_destroy = true
+  # }
+}
+
+resource "azurerm_storage_account" "replacement" {
+  name                     = "replacesa${formatdate("YYMMDD", timestamp())}"
+  resource_group_name      = azurerm_resource_group.standard.name
+  location                 = azurerm_resource_group.standard.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    Environment = var.environment
+    Purpose     = "Replacement"
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
